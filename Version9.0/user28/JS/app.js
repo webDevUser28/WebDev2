2d97d832beae00810b4ea66ff8c0a320349e495d
webDevUser28 <90270110+webDevUser28@users.noreply.github.com>
2021-11-10 11:27:33 -0600

    Update app.js
    changed where the image came from

--- Version9.0/user28/JS/app.js
+++ Version9.0/user28/JS/app.js
@@ -45,7 +45,7 @@ function getWeather(){
 
 // DISPLAY WEATHER TO UI
 function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    iconElement.innerHTML = `<img src="images/${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
